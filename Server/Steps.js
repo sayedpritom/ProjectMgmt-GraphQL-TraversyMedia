b02818988715c6053070d563e2ed908ccea7fbd1
unknown <sayedpritom999@gmail.com>
2022-08-10 01:10:26 +0600

    commit

---
+++ Server/Steps.js
@@ -0,0 +1,8 @@
+/**
+ * 1. npm i express express-graphql graphql mongoose cors colors
+ * 2. npm i -D nodemon dotenv
+ * 3. create "server" folder for models, queries, schema. Put index.js inside server folder
+ * 4. Setup server
+ * app.use in index.js, it will have the '/graphql' url
+ * there should be a sample data 
+*/
